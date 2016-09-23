a90eb7d3597057ef657dbb4a7a9149d69a9fdca1
AnitaLviv <annet_v@ukr.net>
2016-09-23 20:29:40 +0300

    HOME3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,60 @@
-var array = [];
-for(var i = 0; i < 5; i++) {
-    var name = prompt('Введите имя', '');
-    array.push(name);
-}
 
-var userSearch = prompt('Укажите свое имя','');
-var flag = false;
 
-for(var i = 0; i < array.length; i++) {
-    if (array[i] === userSearch) {
-        flag = true;
+
+var dom = {
+    //fields
+    divText      : "Тест по програмуванню",
+    parText      : ". Запитання № ",
+    labelText    : "Варіант відповіді № ",
+    buttonText   : "Перевірити мої результат",
+    
+
+    
+    addBR        : function () {
+        document.body.appendChild(document.createElement('br'));
+    },
+
+    addCheckBox  : function () {
+        for (var i = 1; i <= 3; i += 1) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'checkbox');
+            var text = this.labelText + i;
+            document.body.appendChild(input);
+            document.body.appendChild(document.createTextNode(text));
+            this.addBR();
+        }
+    },
+
+
+    addParagraph : function () {
+        for (var i = 1; i <= 3; i += 1) {
+            var p = document.createElement('p');
+            p.innerHTML = "<p>" + i  + this.parText + i + "</p>";
+            document.body.appendChild(p);
+            this.addCheckBox();
+            this.addBR();
+            this.addBR();
+        }
+    },
+
+    addButton    : function () {
+        var button = document.createElement('button');
+        button.innerHTML = this.buttonText;
+        this.addBR();
+        document.body.appendChild(button);
+    },
+
+    addDiv       : function () {
+        var div = document.createElement('div');
+        div.innerHTML = '<h1>'+ this.divText  +  '</h1>';
+        document.body.appendChild(div);
     }
-}
+    
 
 
-if(flag) {
-    alert( userSearch + ' ' +  'Вы успешно вошли');
-}else {
-    alert('Ошибка');
-}
+};
+
+dom.addDiv();
+dom.addParagraph();
+dom.addButton();
 
